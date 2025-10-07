dfa0f07141bf226962ddc934e4eb78c800ce601a
Abhishek_K <abhishekkarayil@gmail.com>
2025-10-07 17:36:57 +0530

    enhance(Home): update text sizes and spacing for improved readability and layout

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -19,13 +19,13 @@ const Home = () => {
 
         <div className='flex pt-25 w-full md:flex-row lg:flex-row flex-col lg:space-x-20 lg:space-y-0 space-y-6 '>
           <div className='flex flex-col w-full space-y-4'>
-            <h1 className=' text-gray-600 lg:text-5xl text-3xl font-medium leading-snug'>Hi there! I'm Abhishek.</h1>
-            <p className='lg:text-xl text-l max-w-130 text-[#131313] leading-relaxed mt-4 '><span className='font-semibold'>Web Developer</span> with 1 year of expeariance creating responsive, user-friendly app using react.</p>
+            <h1 className=' text-gray-600 lg:text-5xl text-2xl font-medium leading-snug'>Hi there! I'm Abhishek.</h1>
+            <p className='lg:text-xl text-sm max-w-130 text-[#131313] leading-relaxed mt-4 '><span className='font-semibold'>Web Developer</span> with 1 year of expeariance creating responsive, user-friendly app using react.</p>
           </div>
-          <div className='flex flex-col lg:items-end w-full md:items-end md:text-right lg:text-right text-[#5B5B5B] text-sm space-y-2 font-mono'>
+          <div className='flex flex-col lg:items-end text-sm  w-full md:items-end md:text-right lg:text-right text-[#5B5B5B]  space-y-2 font-mono'>
             <p>CURENT LOCATION: INDIA</p>
             <p className='flex items-center '>
-              <span className='w-2 h-2 bg-green-500 rounded-full mr-2'></span>LOCAL TIME: <Time/></p>
+              <span classNsmame='w-2 h-2 bg-green-500 rounded-full mr-2'></span>LOCAL TIME: <Time/></p>
 
           </div>
         </div>
@@ -33,7 +33,7 @@ const Home = () => {
         <div className='w-full flex  relative  mt-5 '>
           <img src={bgImage} alt="Background"  loading="lazy"decoding="async" className='bg-cover grayscale-100 w-full lg:h-70' />
 
-          <div className=' absolute  flex gap-[7px] ml-1 text-xs  md:gap-6 md:ml-3 lg:gap-8 lg:text-base  md:mt-1 text-gray-600 lg:ml-6 rounded-3xl lg:p-2'>
+          <div className=' absolute  flex gap-[9px] ml-2 mt-1 text-[6px]  md:gap-6 md:ml-3 lg:gap-8 lg:text-base  md:mt-1 text-gray-600 lg:ml-6 rounded-3xl lg:p-2'>
             <a href="https://instagram.com" target='_blank' rel='noreferrer'>
               <FaInstagram className='hover:text-black' /> </a>
             <a href="https://twitter.com" target='_blank' rel='noreferrer'>
@@ -41,8 +41,8 @@ const Home = () => {
             <a href="https://linkedin.com" target='_blank' rel='noreferrer'>
               <FaLinkedinIn className='hover:text-black' /></a>
           </div>
-          <img src={ptImage} alt="abhishek" oading="lazy"decoding="async" className='absolute lg:w-[300px] h-31 ml-55 md:w-50 md:h-[245px] md:ml-100 bottom-[.8px] lg:h-[350px] lg:ml-180 lg:bottom-[0.5px]' />
-          <button className='absolute bg-[#4A4A4A] text-white text-[9px] px-3 py:1.5 outline-4
+          <img src={ptImage} alt="abhishek" oading="lazy"decoding="async" className='absolute lg:w-[300px] h-26 ml-45 md:w-50 md:h-[245px] md:ml-100 bottom-[1px] lg:h-[350px] lg:ml-180 lg:bottom-[0.5px]' />
+          <button className='absolute bg-[#4A4A4A] text-white text-[6px] px-3 py:1.5 outline-4
            md:text-xs md:px-6 md:py-1 md:right-2 lg:text-xl lg:px-10 lg:py-1 rounded-4xl bottom-0 right-1 outline-white md:outline-7 lg:outline-5 hover:bg-[#464444] hover:cursor-pointer'>Say Hello</button>
           
         </div>
@@ -51,7 +51,7 @@ const Home = () => {
       <div class="text-center mt-6 flex flex-col items-center gap-1">
     <p class="text-sm text-gray-600">Scroll Down</p>
     <div className="w-32 h-[1.5px] bg-gradient-to-r from-transparent via-gray-400 to-transparent"></div>
-    <FaAnglesDown className='text-xl text-gray-600 mt-2 animate-bounce' />
+    <FaAnglesDown className='lg:text-xl md:text-lg text:-base text-gray-600 mt-2 animate-bounce' />
   </div>
     </section>
 
